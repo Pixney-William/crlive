3e2e375a3ccb6bd2f98b981d59ac7f2cf0d2d8a1
William Åström <william@pixney.com>
2021-04-16 02:18:30 +0200

    first

---
+++ precache-manifest.fa62188c75b35ee71b9ee414e740446a.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a2abcd7fe42b065428",
+    "url": "/css/about.4085853b.css"
+  },
+  {
+    "revision": "0a8bae1785307359b6a1",
+    "url": "/css/app.8a483385.css"
+  },
+  {
+    "revision": "9c88cedb671678590106f711f3d1c3c5",
+    "url": "/fonts/ClashGrotesk-Bold.eot"
+  },
+  {
+    "revision": "f4a3a28bbf9b5d6c54568042e5cf773e",
+    "url": "/fonts/ClashGrotesk-Bold.ttf"
+  },
+  {
+    "revision": "79b4347d6f379eb53b573a3ae3640f36",
+    "url": "/fonts/ClashGrotesk-Bold.woff"
+  },
+  {
+    "revision": "91d3fcb27c40cbf4dd17b92a5144f1b6",
+    "url": "/fonts/ClashGrotesk-Bold.woff2"
+  },
+  {
+    "revision": "b00d4af9a1161bb3774a25ad3e927ccc",
+    "url": "/fonts/ClashGrotesk-Extralight.eot"
+  },
+  {
+    "revision": "3a0f8f0e5225115fbfbd133a376e7956",
+    "url": "/fonts/ClashGrotesk-Extralight.ttf"
+  },
+  {
+    "revision": "e0f7cfd17919b946293a9a75b55cdd3a",
+    "url": "/fonts/ClashGrotesk-Extralight.woff"
+  },
+  {
+    "revision": "64c16a747abfbf65aafb7892db7582ff",
+    "url": "/fonts/ClashGrotesk-Extralight.woff2"
+  },
+  {
+    "revision": "1c765272d4afac9aa8c24b6a306fabb5",
+    "url": "/fonts/ClashGrotesk-Light.eot"
+  },
+  {
+    "revision": "855cc680cf1ae8aab6ffb58885db9d91",
+    "url": "/fonts/ClashGrotesk-Light.ttf"
+  },
+  {
+    "revision": "ef92021d97a05b59bc47363db3e176f3",
+    "url": "/fonts/ClashGrotesk-Light.woff"
+  },
+  {
+    "revision": "a89ccc8a3c7cca1f58ade1876f28db88",
+    "url": "/fonts/ClashGrotesk-Light.woff2"
+  },
+  {
+    "revision": "691298a8a765bcfb24ed1e6ae87804d1",
+    "url": "/fonts/ClashGrotesk-Medium.eot"
+  },
+  {
+    "revision": "32459ccc3954e6b3b04d878e569436b8",
+    "url": "/fonts/ClashGrotesk-Medium.ttf"
+  },
+  {
+    "revision": "03b706ff7b576c215bcd01806a1cb8f7",
+    "url": "/fonts/ClashGrotesk-Medium.woff"
+  },
+  {
+    "revision": "c5366ab4b4317fab075b0104df5be11c",
+    "url": "/fonts/ClashGrotesk-Medium.woff2"
+  },
+  {
+    "revision": "a39731dcfd064b643b8467f0aa1b56bf",
+    "url": "/fonts/ClashGrotesk-Regular.eot"
+  },
+  {
+    "revision": "3b72d0148a613e8ca6937289f9b78ec9",
+    "url": "/fonts/ClashGrotesk-Regular.ttf"
+  },
+  {
+    "revision": "ccb6c96c53ccdd262967988aefce439e",
+    "url": "/fonts/ClashGrotesk-Regular.woff"
+  },
+  {
+    "revision": "6851465c0b1c5e430b43d49203510610",
+    "url": "/fonts/ClashGrotesk-Regular.woff2"
+  },
+  {
+    "revision": "90089c14a44f615b7faf645b82078673",
+    "url": "/fonts/ClashGrotesk-Semibold.eot"
+  },
+  {
+    "revision": "4ff0d5b8b007d784c3e89eef74116833",
+    "url": "/fonts/ClashGrotesk-Semibold.ttf"
+  },
+  {
+    "revision": "294aba2b78e93320014e31a766ad4122",
+    "url": "/fonts/ClashGrotesk-Semibold.woff"
+  },
+  {
+    "revision": "93cce52953a6ded545bf0de4f1b33e15",
+    "url": "/fonts/ClashGrotesk-Semibold.woff2"
+  },
+  {
+    "revision": "e5154121108d8db88cf1cbeddfad2e0a",
+    "url": "/fonts/ClashGrotesk-Variable.eot"
+  },
+  {
+    "revision": "1a99e142c3a3bb6ebd915cde0c207901",
+    "url": "/fonts/ClashGrotesk-Variable.ttf"
+  },
+  {
+    "revision": "0d956387d54108c33686fd305d80ef33",
+    "url": "/fonts/ClashGrotesk-Variable.woff"
+  },
+  {
+    "revision": "218f4f81bdee5932a127929c6d693f0c",
+    "url": "/fonts/ClashGrotesk-Variable.woff2"
+  },
+  {
+    "revision": "2a3876f13d6b0a8aa38ff0b5d5e4e5ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5a2abcd7fe42b065428",
+    "url": "/js/about.05ece386.js"
+  },
+  {
+    "revision": "0a8bae1785307359b6a1",
+    "url": "/js/app.3dc1a1b4.js"
+  },
+  {
+    "revision": "7bd4d7703ecafe913401",
+    "url": "/js/chunk-vendors.b4d01c2b.js"
+  },
+  {
+    "revision": "02d95d92c536e9b3c04698d2a05dccdc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a2ea2fc7228abf5e3332790e2285cbe7",
+    "url": "/svgs/Coinleie.no.svg"
+  },
+  {
+    "revision": "6f80672e47c8aab5444608197d4751c3",
+    "url": "/svgs/Group 1.svg"
+  },
+  {
+    "revision": "7155a21773e4b8fc17277ae8dfa2e13e",
+    "url": "/svgs/bg.svg"
+  },
+  {
+    "revision": "4d474a083f2fb74b1999bed84c97fbb2",
+    "url": "/svgs/logo.svg"
+  },
+  {
+    "revision": "2914ec763f72939ab3cfa7442dea3fa2",
+    "url": "/svgs/missing.svg"
+  }
+]);
